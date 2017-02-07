65aaf056f77efad281413ab22e7e30e33bc3c4b5
Keisuke Saotome <Keisuke@keisukes-MacBook-Air.local>
2017-02-07 22:33:48 +0900

    Set user association with topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -15,6 +15,7 @@ class TopicsController < ApplicationController
 
   def create
     @topic = Topic.create(topics_params)
+    @topic.user_id = current_user.id
     if @topic.save
      redirect_to topics_path, notice: "投稿完了しました"
      NoticeMailer.sendmail_topic(@topic).deliver
