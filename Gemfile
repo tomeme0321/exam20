badd03c5f05133fb3e86e47022aa532e6c4ab233
Keisuke Saotome <Keisuke@keisukes-MacBook-Air.local>
2017-02-07 23:24:42 +0900

    Set Facebook and Twitter IDs and Passwords

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'letter_opener_web'
+  gem 'dotenv-rails'
 end
 
 gem 'rails_12factor', group: :production
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
   #get 'contacts' => 'conatcts#index'
   #get 'topics' => "topics#index"
   resources :topics, only: [:index, :new, :create, :edit, :update, :destroy] do
