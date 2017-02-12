eba5206662c46004154240e32e17f7958f6a2bf9
Keisuke Saotome <Keisuke@keisukes-MacBook-Air.local>
2017-02-12 15:11:42 +0900

    Added comment edit function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,22 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    render :index
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to topic_path(@comment.topic)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:topic_id, :content)
